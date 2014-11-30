ac635013e55c0696114db49aaa3ec7bd97dc901f
Arthur Bressan <arthurpbressan@gmail.com>
2014-11-30 19:19:17 -0200

    Layout do item

--- app/src/main/java/arthur/ddmo/mobilebudget/views/TransactionView.java
+++ app/src/main/java/arthur/ddmo/mobilebudget/views/TransactionView.java
@@ -14,11 +14,9 @@ import arthur.ddmo.mobilebudget.R;
  */
 public class TransactionView extends LinearLayout {
 
-    private final TextView labelValue;
     private final TextView transactionValue;
     private final TextView labelDate;
     private final TextView transactionDate;
-    private final Button buttonEditTransaction;
 
     public TransactionView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,11 +24,8 @@ public class TransactionView extends LinearLayout {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.transaction_view, this);
 
-        labelValue = (TextView) findViewById(R.id.labelValue);
         transactionValue = (TextView) findViewById(R.id.transactionValue);
         labelDate = (TextView) findViewById(R.id.labelDate);
         transactionDate = (TextView) findViewById(R.id.transactionDate);
-
-        buttonEditTransaction = (Button) findViewById(R.id.buttonEditTransaction);
     }
 }
