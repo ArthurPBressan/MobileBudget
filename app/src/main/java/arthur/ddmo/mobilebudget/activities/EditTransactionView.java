3fb51110a87a855f997a3d80443ab0e4ebf7e0c6
Arthur Bressan <arthurpbressan@gmail.com>
2014-11-30 21:51:26 -0200

    Atividade de edição de transações e muda o lugar do click handler da transação

--- app/src/main/java/arthur/ddmo/mobilebudget/activities/EditTransactionView.java
+++ app/src/main/java/arthur/ddmo/mobilebudget/activities/EditTransactionView.java
@@ -2,40 +2,24 @@ package arthur.ddmo.mobilebudget.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import arthur.ddmo.mobilebudget.R;
 
-public class MainActivity extends Activity {
+public class EditTransactionView extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        LinearLayout scrollViewContent = (LinearLayout) findViewById(R.id.transactionsScrollViewContainer);
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        for (int i = 0; i<50; i++) {
-            LinearLayout tv = (LinearLayout) layoutInflater.inflate(R.layout.transaction_view, scrollViewContent);
-            tv.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast t = Toast.makeText(getApplicationContext(), "asdasdasdasddas", Toast.LENGTH_SHORT);
-                    t.show();
-                }
-            });
-        }
+        setContentView(R.layout.activity_edit_transaction_view);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_edit_transaction_view, menu);
         return true;
     }
 
--- app/src/main/java/arthur/ddmo/mobilebudget/views/TransactionView.java
+++ app/src/main/java/arthur/ddmo/mobilebudget/views/TransactionView.java
@@ -15,24 +15,24 @@ import arthur.ddmo.mobilebudget.R;
  */
 public class TransactionView extends LinearLayout {
 
-    private final TextView transactionValue;
-    private final TextView transactionDate;
+    private TextView transactionValue;
+    private TextView transactionDate;
 
     public TransactionView(final Context context, AttributeSet attrs) {
         super(context, attrs);
+        setUp(context);
+    }
 
+    public TransactionView(Context context) {
+        super(context);
+        setUp(context);
+    }
+
+    private void setUp(Context context) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.transaction_view, this);
 
         transactionValue = (TextView) findViewById(R.id.transactionValue);
         transactionDate = (TextView) findViewById(R.id.transactionDate);
-
-        this.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast t = Toast.makeText(context, "asdasdasdasddas", Toast.LENGTH_SHORT);
-                t.show();
-            }
-        });
     }
 }
