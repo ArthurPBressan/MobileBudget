8d38176e0ef59593c5d16c423f7bbb6c25e31556
Arthur Bressan <arthurpbressan@gmail.com>
2014-12-07 12:27:49 -0200

    Coisas

--- app/src/main/java/arthur/ddmo/mobilebudget/activities/EditTransactionActivity.java
+++ app/src/main/java/arthur/ddmo/mobilebudget/activities/EditTransactionActivity.java
@@ -31,7 +31,7 @@ public class EditTransactionActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_save_transaction) {
             return true;
         }
 
--- app/src/main/java/arthur/ddmo/mobilebudget/activities/MainActivity.java
+++ app/src/main/java/arthur/ddmo/mobilebudget/activities/MainActivity.java
@@ -48,7 +48,12 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_new_transaction) {
+            System.out.println("New transaction");
+            return true;
+        }
+        if (id == R.id.action_open_reports) {
+            System.out.println("Reports");
             return true;
         }
 
